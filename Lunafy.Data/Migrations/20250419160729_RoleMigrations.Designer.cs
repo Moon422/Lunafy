6a37bb0c40a887039413fddb9601053c8d045bd0
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2025-04-19 22:07:38 +0600

    RoleMigrations

--- Lunafy.Data/Migrations/20250419160729_RoleMigrations.Designer.cs
+++ Lunafy.Data/Migrations/20250419160729_RoleMigrations.Designer.cs
@@ -4,6 +4,7 @@ using Lunafy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lunafy.Data.Migrations
 {
     [DbContext(typeof(LunafyDbContext))]
-    partial class LunafyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250419160729_RoleMigrations")]
+    partial class RoleMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Lunafy.Data/Migrations/20250419160729_RoleMigrations.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lunafy.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(72)", maxLength: 72, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "RoleUserMappings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleUserMappings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoleUserMappings_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoleUserMappings_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleUserMappings_RoleId",
+                table: "RoleUserMappings",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleUserMappings_UserId",
+                table: "RoleUserMappings",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoleUserMappings");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
