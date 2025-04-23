d7372ead3b3eb33e1410b3e5dc8d8f602d9fa8f2
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2025-04-23 09:15:52 +0600

    sidebar

--- Lunafy.Client/src/components/admin/Sidebar.vue
+++ Lunafy.Client/src/components/admin/Sidebar.vue
@@ -1,115 +1,65 @@
 <script setup lang="ts">
-
+import { HomeIcon, UserIcon } from '@heroicons/vue/16/solid'
 </script>
 
 <template>
-    <button type="button" class="btn btn-text max-sm:btn-square sm:hidden" aria-haspopup="dialog" aria-expanded="false" aria-controls="multilevel-with-separator" data-overlay="#multilevel-with-separator" >
-    <span class="icon-[tabler--menu-2] size-5"></span>
+    <button type="button" class="btn btn-text max-sm:btn-square sm:hidden" aria-haspopup="dialog" aria-expanded="false"
+        aria-controls="multilevel-with-separator" data-overlay="#multilevel-with-separator">
+        <span class="icon-[tabler--menu-2] size-5"></span>
     </button>
 
-    <aside id="multilevel-with-separator" class="overlay [--auto-close:sm] overlay-open:translate-x-0 drawer drawer-start hidden max-w-64 sm:absolute sm:z-0 sm:flex sm:translate-x-0 sm:shadow-none" tabindex="-1" >
-    <div class="drawer-body px-2 pt-4">
-        <ul class="menu space-y-0.5 p-0">
-        <li>
-            <a href="#">
-            <span class="icon-[tabler--home] size-5"></span>
-            Home
-            </a>
-        </li>
-        <li class="space-y-0.5">
-            <a class="collapse-toggle collapse-open:bg-base-content/10" id="menu-app" data-collapse="#menu-app-collapse">
-            <span class="icon-[tabler--apps] size-5"></span>
-            Apps
-            <span class="icon-[tabler--chevron-down] collapse-open:rotate-180 size-4 transition-all duration-300"></span>
-            </a>
-            <ul id="menu-app-collapse" class="collapse hidden w-auto space-y-0.5 overflow-hidden transition-[height] duration-300" aria-labelledby="menu-app" >
-            <li>
-                <a href="#">
-                <span class="icon-[tabler--message] size-5"></span>
-                Chat
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                <span class="icon-[tabler--calendar] size-5"></span>
-                Calendar
-                </a>
-            </li>
-            <li class="space-y-0.5">
-                <a class="collapse-toggle collapse-open:bg-base-content/10" id="sub-menu-academy" data-collapse="#sub-menu-academy-collapse" >
-                <span class="icon-[tabler--book] size-5"></span>
-                Academy
-                <span class="icon-[tabler--chevron-down] collapse-open:rotate-180 size-4"></span>
-                </a>
-                <ul id="sub-menu-academy-collapse" class="collapse hidden w-auto space-y-0.5 overflow-hidden transition-[height] duration-300" aria-labelledby="sub-menu-academy" >
-                <li>
-                    <a href="#">
-                    <span class="icon-[tabler--books] size-5"></span>
-                    Courses
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                    <span class="icon-[tabler--list-details] size-5"></span>
-                    Course details
-                    </a>
-                </li>
-                <li class="space-y-0.5">
-                    <a class="collapse-toggle collapse-open:bg-base-content/10" id="sub-menu-academy-stats" data-collapse="#sub-menu-academy-stats-collapse" >
-                    <span class="icon-[tabler--chart-bar] size-5"></span>
-                    Stats
-                    <span class="icon-[tabler--chevron-down] collapse-open:rotate-180 size-4"></span>
-                    </a>
-                    <ul id="sub-menu-academy-stats-collapse" class="collapse hidden w-auto space-y-0.5 overflow-hidden transition-[height] duration-300" aria-labelledby="sub-menu-academy-stats" >
+    <div class="fixed top-14 left-0 w-64 min-h-[calc(100vh-3.5rem)] z-50 bg-green-500">
+        <button type="button" class="btn btn-text max-sm:btn-square sm:hidden" aria-haspopup="dialog"
+            aria-expanded="false" aria-controls="multilevel-with-separator" data-overlay="#multilevel-with-separator">
+            <span class="icon-[tabler--menu-2] size-5"></span>
+        </button>
+
+        <aside id="multilevel-with-separator"
+            class="overlay [--auto-close:sm] overlay-open:translate-x-0 drawer drawer-start hidden max-w-64 sm:absolute sm:z-0 sm:flex sm:translate-x-0 sm:shadow-none"
+            tabindex="-1">
+            <div class="drawer-body px-2 pt-4">
+                <ul class="menu space-y-0.5 p-0">
                     <li>
                         <a href="#">
-                        <span class="icon-[tabler--chart-donut] size-5"></span>
-                        Goals
+                            <HomeIcon class="size-6" /> Home
                         </a>
                     </li>
-                    </ul>
-                </li>
+                    <li>
+                        <a href="#">
+                            <UserIcon class="size-6" /> Users
+                        </a>
+                    </li>
+                    <div class="divider text-base-content/50 py-6 after:border-0">Content</div>
+                    <li>
+                        <a href="#">
+                            <HomeIcon class="size-6" /> Tracks
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <UserIcon class="size-6" /> Albums
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <HomeIcon class="size-6" /> Artists
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <UserIcon class="size-6" /> Playlists
+                        </a>
+                    </li>
+                    <div class="divider text-base-content/50 py-6 after:border-0">Reports</div>
+                    <li>
+                        <a href="#">
+                            <UserIcon class="size-6" /> Analytics
+                        </a>
+                    </li>
                 </ul>
-            </li>
-            </ul>
-        </li>
-        <li>
-            <a href="#">
-            <span class="icon-[tabler--settings] size-5"></span>
-            Settings
-            </a>
-        </li>
-        <div class="divider text-base-content/50 py-6 after:border-0">Account</div>
-        <li>
-            <a href="#">
-            <span class="icon-[tabler--login] size-5"></span>
-            Sign In
-            </a>
-        </li>
-        <li>
-            <a href="#">
-            <span class="icon-[tabler--logout-2] size-5"></span>
-            Sign Out
-            </a>
-        </li>
-        <div class="divider text-base-content/50 py-6 after:border-0">Miscellaneous</div>
-        <li>
-            <a href="#">
-            <span class="icon-[tabler--users-group] size-5"></span>
-            Support
-            </a>
-        </li>
-        <li>
-            <a href="#">
-            <span class="icon-[tabler--files] size-5"></span>
-            Documentation
-            </a>
-        </li>
-        </ul>
+            </div>
+        </aside>
     </div>
-    </aside>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
