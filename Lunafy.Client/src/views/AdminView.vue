ab6d045e426b55283101c264e90bffbdd598b1f9
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2025-04-26 11:46:27 +0600

    added bootstrap icons

--- Lunafy.Client/src/views/AdminView.vue
+++ Lunafy.Client/src/views/AdminView.vue
@@ -6,95 +6,37 @@ const showSidebar = ref(true)
 </script>
 
 <template>
-    <div class="admin-layout">
+    <div class="">
         <!-- topbar -->
-        <div :class="`topbar ${showSidebar ? 'topbar-open' : 'topbar-close'} bg-primary`">
+        <div class="bg-primary">
             <h1>topbar</h1>
         </div>
 
         <!-- sidebar -->
-        <!-- <div :class="`sidebar ${showSidebar ? 'sidebar-open' : 'sidebar-close'}`">
-            <Sidebar />
-        </div> -->
-
-        <div :class="`main-content ${showSidebar ? 'main-content-open' : 'main-content-close'}`">
-            <h1>fuck you</h1>
-            <button v-on:click="showSidebar = !showSidebar">click me</button>
+        <div class="bg-danger">
+            <!-- <Sidebar /> -->
+            <h1>sidebar</h1>
         </div>
+
+        <div class="bg-success">
+            <h1>fuck you</h1>
+            <button class="btn btn-primary" v-on:click="showSidebar = !showSidebar">click me</button>
+        </div>
+
+        <header class="navbar navbar-expand-lg sticky-top">
+            <nav class="container-xxl gutter flex-wrap flex-lg-nowrap" aria-label="Main navigation">
+                <button class="navbar-toggler p-2" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar"
+                    aria-controls="sidebar" aria-label="Toggle sidebar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <!-- <a href="/admin" class="navbar-brand p-0 me-0 me-lg-2"> -->
+                <RouterLink to="/admin" class="navbar-brand p-0 me-0 me-lg-2">
+                    Lunafy
+                </RouterLink>
+                </a>
+            </nav>
+        </header>
     </div>
 </template>
 
-<style scoped>
-.admin-layout {
-    background: #f9fafb;
-    font-family: 'Inter', 'Noto Sans', 'sans-serif';
-    transition: all 300ms;
-}
-
-.admin-layout.topbar {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    height: 5rem;
-    z-index: 50;
-    display: flex;
-    transition: all 300ms;
-    border: 2px solid black;
-}
-
-.admin-layout.sidebar {
-    position: fixed;
-    top: 0;
-    display: none;
-    width: 16rem;
-    height: 100vh;
-    z-index: 50;
-    display: flex;
-    transition: all 300ms;
-}
-
-.admin-layout.main-content {
-    margin-top: 80rem;
-    margin-left: 0;
-    flex: 1;
-    overflow: auto;
-    padding: 1.25rem;
-    transition: all 300ms;
-    border: 2px solid black;
-}
-
-@media (width >=64rem) {
-    .admin-layout.topbar {
-        left: 16rem;
-    }
-
-    .admin-layout.sidebar {
-        display: block;
-    }
-
-    .admin-layout.topbar-open {
-        margin-left: 16rem;
-    }
-
-    .admin-layout.topbar-close {
-        left: -16rem;
-    }
-
-    .admin-layout.sidebar-open {
-        left: 0;
-    }
-
-    .admin-layout.sidebar-close {
-        margin-left: 0;
-    }
-
-    .admin-layout.main-content-open {
-        margin-left: 16rem;
-    }
-
-    .admin-layout.main-content-close {
-        margin-left: 0;
-    }
-}
-</style>
+<style scoped></style>
