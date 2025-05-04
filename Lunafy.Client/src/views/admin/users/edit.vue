63fe0ea982c8be6161d750e6a56413e081e6cba1
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2025-05-04 18:57:09 +0600

    working on edit page

---
+++ Lunafy.Client/src/views/admin/users/edit.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <!-- Header -->
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+            <h3>Edit User - John Doe</h3>
+            <div class="d-flex">
+                <button class="btn btn-success me-2">
+                    <i class="bi bi-floppy-fill"></i>
+                    Save
+                </button>
+                <button class="btn btn-danger">
+                    <i class="bi bi-trash-fill"></i>
+                    Delete
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="accordion">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
+                        aria-controls="panelsStayOpen-collapseOne">
+                        Accordion Item #1
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                    <div class="accordion-body">
+                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the
+                        collapse plugin adds the appropriate classes that we use to style each element. These classes
+                        control the overall appearance, as well as the showing and hiding via CSS transitions. You can
+                        modify any of this with custom CSS or overriding our default variables. It's also worth noting
+                        that just about any HTML can go within the <code>.accordion-body</code>, though the transition
+                        does limit overflow.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="css" scoped></style>
