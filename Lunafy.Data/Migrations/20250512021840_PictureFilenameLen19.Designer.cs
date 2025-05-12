ebf55a441b678a4c3e2124f34e83ef38840a7a69
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2025-05-12 08:18:50 +0600

    PictureFilenameLen19

--- Lunafy.Data/Migrations/20250512021840_PictureFilenameLen19.Designer.cs
+++ Lunafy.Data/Migrations/20250512021840_PictureFilenameLen19.Designer.cs
@@ -4,6 +4,7 @@ using Lunafy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lunafy.Data.Migrations
 {
     [DbContext(typeof(LunafyDbContext))]
-    partial class LunafyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250512021840_PictureFilenameLen19")]
+    partial class PictureFilenameLen19
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Lunafy.Data/Migrations/20250512021840_PictureFilenameLen19.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lunafy.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PictureFilenameLen19 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Filename",
+                table: "Pictures",
+                type: "char(19)",
+                fixedLength: true,
+                maxLength: 19,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "char(16)",
+                oldFixedLength: true,
+                oldMaxLength: 16)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Filename",
+                table: "Pictures",
+                type: "char(16)",
+                fixedLength: true,
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "char(19)",
+                oldFixedLength: true,
+                oldMaxLength: 19)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
